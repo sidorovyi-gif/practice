f8d4c957b5adcba5ffc0d4f40475b98abc4bd066
zzYaRa <SidorovYI@mer.ci.nsu.ru>
2026-05-20 14:09:56 +0700

    created 3 buttons and some preps for navigation control

--- main/src/main/java/ci/nsu/mobile/main/MainActivity.kt
+++ main/src/main/java/ci/nsu/mobile/main/MainActivity.kt
@@ -1,5 +1,7 @@
 package ci.nsu.mobile.main
 
+import android.R.attr.button
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,10 +15,18 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode.Companion.Screen
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 import ci.nsu.mobile.main.ui.theme.PracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,33 +35,37 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PracticeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
-                }
+                MainActivityScreen()
             }
         }
     }
 }
 
+sealed class LunchTrayScreen(val title: String) {
+    data object Home : LunchTrayScreen("Р")
+    data object ScreenOne : LunchTrayScreen("screenone")
+    data object ScreenTwo : LunchTrayScreen("screentwo")
+}
+
 @Composable
-fun MainScreen(){
+fun MainActivityScreen(){
+    val navController = rememberNavController()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically)
-    ) {
-        Text("🏠 Главный экран", style = MaterialTheme.typography.headlineMedium)
-
-        Button(
-            onClick = { navController.navigate(Screen.Settings.route) },
-            modifier = Modifier.fillMaxWidth()
-        ) { Text("⚙️ Настройки") }
-
-        Button(
-            onClick = { navController.navigate(Screen.Profile.route) },
-            modifier = Modifier.fillMaxWidth()
-        ) { Text("👤 Профиль") }
+    ){
+        Button( onClick = {  } ) {
+            Text("Рассчитать")
+        }
+        Button( onClick = {  } ) {
+            Text("История расчётов")
+        }
+        Button( onClick = {  } ) {
+            Text("Закрыть приложение")
+        }
     }
 }
