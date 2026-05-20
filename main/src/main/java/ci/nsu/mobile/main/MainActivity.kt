da83e1167792211fbb7d318929ecb3fce3ace0d1
zzYaRa <SidorovYI@mer.ci.nsu.ru>
2026-05-20 13:28:16 +0700

    Start OF the lab

---
+++ main/src/main/java/ci/nsu/mobile/main/MainActivity.kt
@@ -0,0 +1,57 @@
+package ci.nsu.mobile.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ci.nsu.mobile.main.ui.theme.PracticeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PracticeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically)
+    ) {
+        Text("🏠 Главный экран", style = MaterialTheme.typography.headlineMedium)
+
+        Button(
+            onClick = { navController.navigate(Screen.Settings.route) },
+            modifier = Modifier.fillMaxWidth()
+        ) { Text("⚙️ Настройки") }
+
+        Button(
+            onClick = { navController.navigate(Screen.Profile.route) },
+            modifier = Modifier.fillMaxWidth()
+        ) { Text("👤 Профиль") }
+    }
+}
