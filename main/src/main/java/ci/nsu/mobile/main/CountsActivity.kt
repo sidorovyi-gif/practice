b8435c40ee97c29f89b45f0284733fb6f77a8aa4
zzYaRa <SidorovYI@mer.ci.nsu.ru>
2026-05-27 14:31:25 +0700

    REWRITE ALL THAT WAS BEFORE, BEGIN FROM SCRATCH, KILL THAT NIGGA OVER THERE AND REORGANIZE HIS ORGANS
    made 3 buttons and made them move you to the other Activity where will be the main action :3

---
+++ main/src/main/java/ci/nsu/mobile/main/CountsActivity.kt
@@ -0,0 +1,78 @@
+package ci.nsu.mobile.main
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SegmentedButtonDefaults.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ci.nsu.mobile.main.ui.ui.theme.PracticeTheme
+
+class CountsActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PracticeTheme() {
+                CountScreen()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CountScreen() {
+    var receivedText by remember { mutableStateOf("") }
+    val context = LocalContext.current
+
+    Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
+        TopAppBar(
+            title = { Text(receivedText) }, navigationIcon = {
+                IconButton(onClick = {
+                    val intent = Intent(context, MainActivity::class.java).apply{}
+                    if (context is Activity) {
+                        context.finish()
+                    }
+                }) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "Back",
+                        tint = Color.White
+                    )
+                }
+            }, colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = Color.Blue, titleContentColor = Color.White
+            )
+        )
+    }) {innerPadding ->
+
+    }
+}
--- main/src/main/java/ci/nsu/mobile/main/MainActivity.kt
+++ main/src/main/java/ci/nsu/mobile/main/MainActivity.kt
@@ -1,20 +1,19 @@
 package ci.nsu.mobile.main
 
-import android.R.attr.button
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -22,17 +21,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.BlendMode.Companion.Screen
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.rememberNavController
-import ci.nsu.mobile.main.ui.theme.PracticeTheme
-import ci.nsu.mobile.main.ui.theme.screens.FirstBox
-import ci.nsu.mobile.main.ui.theme.screens.MainScreen
-import ci.nsu.mobile.main.ui.theme.screens.SecondBox
-import kotlinx.coroutines.MainScope
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,10 +30,53 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme{
-                Surface(modifier = Modifier.fillMaxSize()){
-                    MainScreen()
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MainActivityScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun MainActivityScreen(modifier: Modifier) {
+    var text by remember { mutableStateOf("") }
+    val context = LocalContext.current as? Activity
+    Column(
+        modifier = modifier.fillMaxSize().padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = {
+                val intent = Intent(context, CountsActivity::class.java).apply{
+                    putExtra("text_data", text)
+                }
+                context?.startActivity(intent)
+            },
+            modifier = Modifier.padding(top = 16.dp)
+        ) {
+            Text("Counts")
+        }
+        Button(
+            onClick = {
+                val intent = Intent(context, HistoryActivity::class.java).apply{
+                    putExtra("text_data", text)
+                }
+                context?.startActivity(intent)
+            },
+            modifier = Modifier.padding(top = 16.dp)
+        ) {
+            Text("History")
+        }
+        Button(
+            onClick = {
+                (context as? Activity)?.finishAndRemoveTask()
+            },
+            modifier = Modifier.padding(top = 16.dp)
+        ) {
+            Text("Exit")
         }
     }
 }
